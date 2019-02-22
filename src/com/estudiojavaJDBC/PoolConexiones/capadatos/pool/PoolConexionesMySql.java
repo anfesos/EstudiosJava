51be3fa2d798ac55d3977ff0ceffd30adc7bd1fb
Sossa <andres.sossa@accenture.com>
2019-02-22 16:36:33 -0500

    Finalizaciòn curso JBDC pool conexiones, cursores, y comienzo curso HTML

---
+++ src/com/estudiojavaJDBC/PoolConexiones/capadatos/pool/PoolConexionesMySql.java
@@ -0,0 +1,23 @@
+package com.estudiojavaJDBC.PoolConexiones.capadatos.pool;
+
+import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class PoolConexionesMySql {
+	
+	public static DataSource getDataSource() {
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUsername("root");
+		ds.setPassword("admin");
+		ds.setUrl("jdbc:mysql://localhost:3306/sga?useSSL=false");
+		//Definimos el tamaño del pool de conexiones
+		ds.setInitialSize(5);//5 conexiones iniciales
+		return ds;
+	}
+	
+	public static Connection getConexion() throws SQLException{
+		return getDataSource().getConnection();
+	}
+}
